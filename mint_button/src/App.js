a61fc0dfcdd0d8ddf385b661916259b36fe4ea18
YDC <miniming1026@naver.com>
2023-01-05 18:16:51 +0900

    저장

---
+++ mint_button/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// import Header from './Header';
+import './css/Reset.css';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <div className='content'>
+            <ul>
+              <li><h1><a href="#"></a></h1></li>
+              <li><a href="community">커뮤니티</a></li>
+              <li><a href="collection">스위치</a></li>
+              <li><a href=""></a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section>
+          
+
+        </section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;

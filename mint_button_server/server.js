c8fa39771df8aa7b0c4d9f9158e553aa98485404
ydc <miniming1026@naver.com>
2023-01-13 01:39:36 +0900

    mongo 세팅

--- mint_button_server/server.js
+++ mint_button_server/server.js
@@ -6,9 +6,26 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended : true }))
 
+
+const MongoClient = require('mongodb').MongoClient;
+
+// //자료 담을 db 미리 선언해둠 
+var db;
+MongoClient.connect('mongodb+srv://miniming1026:flqhdaos12@cluster0.ausvy.mongodb.net/Todoapp?retryWrites=true&w=majority',function(에러,client){
+
+if (에러) return console.log(에러)
+
 app.listen(8080, function() {
-    console.log('listening on 8080')
+  console.log('listening on 8080')
 })
+
+db = client.db('todoapp');
+
+db.collection('post').insertOne( {이름 : 'John', _id : 100} , function(에러, 결과){
+    console.log('저장완료'); 
+});
+
+});
 
 app.get('/', function(요청, 응답) { 
   응답.sendFile(__dirname +'/index.html');

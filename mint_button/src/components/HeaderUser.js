2c1bc14ec7422e8eed159e6726ed383c88b00d4b
ydc <miniming1026@naver.com>
2023-01-12 23:55:37 +0900

    익스프레스 세팅

--- mint_button/src/components/HeaderUser.js
+++ mint_button/src/components/HeaderUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from 'react';
+import { Link } from "react-router-dom";
 
 import '../css/HeaderUser.css';
 
@@ -8,8 +9,15 @@ function HeaderUser () {
     return (
     <ul id="render_user" className='render_user_on'>
         {/* 회원가입/내정보 */}
-        <li><a href="#"> { a } </a></li>
-        <li><a href="#">로그인</a></li>
+
+        <li>
+            <Link to="/user_join">{ a }</Link>
+        </li>
+
+        <li>
+            <Link to="/user_login"> 로그인 </Link>
+        </li>
+
     </ul>
     )
 }
--- mint_button_server/server.js
+++ mint_button_server/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser= require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
 
-MongoClient.connect('님 접속 URL', function(에러, client){
-  if (에러) return console.log(에러)
-  app.listen(8080, function() {
+
+// 인풋정보 불러올 때 사용
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended : true }))
+
+app.listen(8080, function() {
     console.log('listening on 8080')
-  })
 })
 
 app.get('/', function(요청, 응답) { 
@@ -21,5 +20,7 @@ app.get('/write', function(요청, 응답) {
 
 app.post('/add', function(요청, 응답){
   console.log(요청.body);
+  console.log(요청.body.date);
+  console.log(요청.body.title);
   응답.send('전송완료');
 });
